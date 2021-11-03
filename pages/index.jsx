adfa4c5dfe65c0b6cde2e43712896da58d0920d3
buezrico <ricobuez@gmail.com>
2021-11-03 01:28:49 +0100

    added particles

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ import Navbar from "../components/Navbar";
 import TechSlideComp from "../components/TechSlideComp";
 import { Anchor } from "antd";
 import dynamic from "next/dynamic";
+import Particles from "react-particles-js";
 
 const CrispWithNoSSR = dynamic(() => import("../components/Crisp"), {
   ssr: false,
@@ -14,6 +15,16 @@ const CrispWithNoSSR = dynamic(() => import("../components/Crisp"), {
 gsap.registerPlugin(TextPlugin);
 
 export default function Home() {
+  // const particlesInit = (main) => {
+  //   console.log(main);
+
+  //   // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  // };
+
+  // const particlesLoaded = (container) => {
+  //   console.log(container);
+  // };
+
   const [dark, setDark] = useState(false);
   // const [view, setView] = useState(false);
   const { Link } = Anchor;
@@ -86,6 +97,126 @@ export default function Home() {
   }, []);
   return (
     <div className="overall">
+      <Particles
+        id="particles"
+        params={{
+          particles: {
+            number: {
+              value: 80,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            color: {
+              value: "#ffffff",
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0,
+                color: "#000000",
+              },
+              polygon: {
+                nb_sides: 5,
+              },
+              image: {
+                src: "img/github.svg",
+                width: 100,
+                height: 100,
+              },
+            },
+            opacity: {
+              value: 0.5,
+              random: false,
+              anim: {
+                enable: false,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false,
+              },
+            },
+            size: {
+              value: 5,
+              random: true,
+              anim: {
+                enable: false,
+                speed: 40,
+                size_min: 0.1,
+                sync: false,
+              },
+            },
+            line_linked: {
+              enable: true,
+              distance: 150,
+              color: "#ffffff",
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 2,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+              },
+            },
+          },
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onhover: {
+                enable: true,
+                mode: "grab",
+              },
+              onclick: {
+                enable: true,
+                mode: "push",
+              },
+              resize: true,
+            },
+            modes: {
+              grab: {
+                distance: 400,
+                line_linked: {
+                  opacity: 1,
+                },
+              },
+              bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 8,
+                speed: 3,
+              },
+              repulse: {
+                distance: 200,
+              },
+              push: {
+                particles_nb: 4,
+              },
+              remove: {
+                particles_nb: 2,
+              },
+            },
+          },
+          retina_detect: true,
+          config_demo: {
+            hide_card: false,
+            background_color: "#b61924",
+            background_image: "",
+            background_position: "50% 50%",
+            background_repeat: "no-repeat",
+            background_size: "cover",
+          },
+        }}
+      />
+
       {/* <div className={`${view ? "d-none" : ""} mode container`}>
         <img src="/images/pp.png" alt="" />
         <h2 className="mode-title"></h2>
